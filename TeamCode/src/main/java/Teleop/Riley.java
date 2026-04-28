5b39f7cb800b6f5c71aee402871e47a8edc544a4
EJS70 <ejs70@students.needham.k12.ma.us>
2026-04-28 19:19:13 -0400

    yo

--- TeamCode/src/main/java/Teleop/Riley.java
+++ TeamCode/src/main/java/Teleop/Riley.java
@@ -1,3 +1,5 @@
+package Teleop;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,30 +11,19 @@ import java.lang.Math;
 @Teleop
 public class Riley extends OpMode{
   private DcMotorEx frontLeft, frontRight, backLeft, backRight;
-  private DcMotorEx flywheelRight, flywheelLeft;
-  private CRServo leaderServo;
 
   @Override
   public void init() {
-    telemetry.addData("Heyo World")
-  }
-
-  @Override
-  public void init() {
-    frontLeft = hardwareMap.get(DcMotorEx.class, "fl")
-    frontRight = hardwareMap.get(DcMotorEx.class, "fr")
-    backLeft = hardwareMap.get(DcMotorEx.class, "bl")
-    backRight = hardwareMap.get(DcMotorEx.class, "br")
-    wheelLeft = hardwareMap.get(DcMotorEx.class, "wl")
-    wheelRight = hardwareMap.get(DcMotorEx.class, "wr")
+    frontLeft = hardwareMap.get(DcMotorEx.class, "fl");
+    frontRight = hardwareMap.get(DcMotorEx.class, "fr");
+    backLeft = hardwareMap.get(DcMotorEx.class, "bl");
+    backRight = hardwareMap.get(DcMotorEx.class, "br");
 
     //Determines 
     frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
     backLeft.setDirection(DcMotorEx.Direction.REVERSE);
     frontRight.setDirection(DcMotorEx.Direction.FORWARD);
     frontRight.setDirection(DcMotorEx.Direction.FORWARD);
-    wheelLeft.setDirection.(DcMotor.Direction.FORWARD);
-    wheelRight.setDirection.(DcMotor.Direction.REVERSE);
   }
 
   public void driveOmni(double x, double y, double rx) {
@@ -55,36 +46,11 @@ public class Riley extends OpMode{
 
 
   @Override
-  public void loop() 
-  {
-   //Determines the inputs of the gamepad
-   double y = gamepad1.left_stick_y;
-   double x = gamepad1.left_stick_x;
-   double rx = gamepad1.right_stick_x;
-   driveOmni(y, x, rx);
-   if (gamepad1.x) {
-     wheelRight.setPower(1);
-     wheelLeft.setPower(1);
-     try {
-       //Pauses program
-       Thread.sleep(1500)
-     } catch (InterruptedException e) {
-       throw new RuntimeException(e);
-     }
-   } else if (gamepad1.x) {
-     telemetry.addData("Bonjour!")
-     loaderServo.setPower(1);
-
-     try {
-       //Pauses program
-       Thread.sleep(500)
-     } catch (InterruptedException e) {
-       throw new RuntimeException(e);
-   } else {
-     //Contingency in case of not pressing button to power down flywheels
-     wheelRight.setPower(0);
-     wheelLeft.setPower(0);
-     loaderServo.setPower(0);
-     }
+  public void loop() {
+    //Determines the inputs of the gamepad
+    double y = gamepad1.left_stick_y;
+    double x = gamepad1.left_stick_x;
+    double rx = gamepad1.right_stick_x;
+    driveOmni(y, x, rx);
   }
 }
